21c353276ab20b382934997c5a3fb7a32b656ebd
Scott C. MacCallum <smaccallum@protonmail.com>
2020-05-27 14:22:01 -0400

    Adding file

---
+++ load-betsy.lisp
@@ -0,0 +1,26 @@
+;; load-betsyro.lisp
+
+;; This program is part of the Betsy project, it loads knowledge for
+;; Betsy
+
+;; Copyright (C) 2015, 2016, 2018, 2020 Scott C. MacCallum
+;; smaccallum@protonmail.com
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Affero License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+(load "meet.lisp")
+(load "question.lisp")
+(load "another-question.lisp")
+(load "main.lisp")
+(main)
